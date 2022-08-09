66e1d206940ab44c1e483d47fc07b641f98e02af
Maria Spiridonova <traffic2007@mail.ru>
2022-08-09 19:49:27 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+
+
+int Prompt(string message)
+{
+System.Console.Write(message); // Вывести сообщение
+string readValue = Console.ReadLine(); // Считавает с консоли строку
+int result = int.Parse(readValue); // Преобрадует строку в целое число
+return result; // Возвращает результат
+}
+int number = Prompt("Введите число ");
+int evennumber = number % 2;
+if (evennumber == 0){
+System.Console.WriteLine("Число четное");
+}
+else{
+System.Console.WriteLine("Число нечетное");
+}
